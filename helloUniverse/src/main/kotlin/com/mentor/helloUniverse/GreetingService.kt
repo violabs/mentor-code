3a23dd06527d58761c812f59b4c20fb0e58c07be
Violabs Josh <139578299+violabs-josh@users.noreply.github.com>
2025-03-03 15:00:26 -0600

    Update test to pass based on first requirements

--- helloUniverse/src/main/kotlin/com/mentor/helloUniverse/GreetingService.kt
+++ helloUniverse/src/main/kotlin/com/mentor/helloUniverse/GreetingService.kt
@@ -2,9 +2,17 @@ package com.mentor.helloUniverse
 
 import org.springframework.stereotype.Service
 
+private val METADATA = mapOf(
+    "version" to "1.0",
+    "mode" to "dev"
+)
+
 @Service
 class GreetingService {
     fun getGreeting(): Greeting? {
-        return Greeting()
+        return Greeting(
+            message = "Hello, Universe!",
+            metadata = METADATA
+        )
     }
 }
