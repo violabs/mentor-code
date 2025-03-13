ef6f0798224108176d986825f409f6625e6fce60
Violabs Josh <139578299+violabs-josh@users.noreply.github.com>
2025-03-12 19:36:32 -0500

    Setup success scenario without verification

--- helloUniverse/src/main/kotlin/com/mentor/helloUniverse/controller/GreetingController.kt
+++ helloUniverse/src/main/kotlin/com/mentor/helloUniverse/controller/GreetingController.kt
@@ -1,15 +1,20 @@
 package com.mentor.helloUniverse.controller
 
+import com.mentor.helloUniverse.domain.Greeting
+import com.mentor.helloUniverse.service.GreetingService
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("greeting")
-class GreetingController {
+class GreetingController(
+    @Autowired val greetingService: GreetingService
+) {
 
     @GetMapping
-    fun greet(): Any? {
-        return null
+    fun greet(): Greeting? {
+        return greetingService.getGreeting()
     }
 }
--- helloUniverse/src/test/kotlin/com/mentor/helloUniverse/controller/GreetingControllerTests.kt
+++ helloUniverse/src/test/kotlin/com/mentor/helloUniverse/controller/GreetingControllerTests.kt
@@ -1,19 +1,27 @@
 package com.mentor.helloUniverse.controller
 
 import com.mentor.helloUniverse.TestStyle
+import com.mentor.helloUniverse.domain.Greeting
+import com.mentor.helloUniverse.service.GreetingService
+import io.mockk.every
+import io.mockk.mockk
 import org.junit.jupiter.api.Test
 
 class GreetingControllerTests {
-    private val greetingController = GreetingController()
+    private val greetingService = mockk<GreetingService>()
+
+    private val greetingController = GreetingController(greetingService)
 
     @Test
     fun `greet will return default 'Hello Universe!' if no name is provided`() = TestStyle.bdd {
         given("no name provided") {
+            every { greetingService.getGreeting() } returns Greeting("Hello Universe!")
+
             whenever("we call the endpoint with no name provided") {
                 val actual = greetingController.greet()
 
                 then("expect 'Hello Universe!'") {
-                    val expected = "Hello Universe!"
+                    val expected = Greeting("Hello Universe!")
 
                     assert(actual == expected) {
                         """
